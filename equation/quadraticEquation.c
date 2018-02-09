a0b7225148cf83c818ae9301f01aff6fab462f40
David17Han <3210687009@qq.com>
2018-02-09 14:52:48 +0800

    add comment in quadraticEquation.*

--- equation/quadraticEquation.c
+++ equation/quadraticEquation.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-double x1 , x2 ;
+double x1 , x2 ;//solution of equation
 int solutionNum ;
+
+/* solve equation ax^2+bx+c=0, return the solution number:0,1 or 2 */
 int solveQuadraticEquation( double a , double b , double c)
 {
     double delta ;
@@ -20,6 +22,18 @@ int solveQuadraticEquation( double a , double b , double c)
     x2 =(-b+sqrt(delta))/2;
     return solutionNum ;
 }
+/* Get the solution one by one after calling solveQuadraticEquation
+   you should get the solution like the following :
+   int soln =  solveQuadraticEquation( 1.0,-3.0,2.0 ) ;
+   if ( soln == 2 ) {
+        double x1 = getQuadraticEquationSolution() ;
+        double x2 = getQuadraticEquationSolution() ;
+   }else if( soln == 1 ) {
+        double x1 = getQuadraticEquationSolution() ;
+   }else {
+        //no solution
+   }
+*/
 double getQuadraticEquationSolution()
 {
     double x = 0 ;
@@ -38,7 +52,7 @@ double getQuadraticEquationSolution()
     return x ;
 }
 
-/*
+/*  //main for test
 int main()
 {
     double a,b,c ;
--- equation/quadraticEquation.h
+++ equation/quadraticEquation.h
@@ -1,3 +1,16 @@
 
+/* solve equation ax^2+bx+c=0, return the solution number:0,1 or 2 */
 extern int solveQuadraticEquation( double a , double b , double c) ;
+/* Get the solution one by one after calling solveQuadraticEquation
+   you should get the solution like the following :
+   int soln =  solveQuadraticEquation( 1.0,-3.0,2.0 ) ;
+   if ( soln == 2 ) {
+        double x1 = getQuadraticEquationSolution() ;
+        double x2 = getQuadraticEquationSolution() ;
+   }else if( soln == 1 ) {
+        double x1 = getQuadraticEquationSolution() ;
+   }else {
+        //no solution
+   }
+*/
 extern double getQuadraticEquationSolution() ;
