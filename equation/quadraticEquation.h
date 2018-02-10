40c732761abc53e0c3501d161874a346ce800c57
Lion <3210687009@qq.com>
2018-02-11 00:30:07 +0800

    improve head file

--- equation/quadraticEquation.h
+++ equation/quadraticEquation.h
@@ -6,8 +6,8 @@ extern int solveQuadraticEquation( double a , double b , double c) ;
    you should get the solution like the following :
    int soln =  solveQuadraticEquation( 1.0,-3.0,2.0 ) ;
    if ( soln == 2 ) {
-        double x1 = getQuadraticEquationSolution(1) ;
-        double x2 = getQuadraticEquationSolution(2) ;
+        double x1 =  getQuadraticEquationSolution(1) ;
+        double x2 =  getQuadraticEquationSolution(2) ;
    }else {
         //no solution 
    }
